e3e2f8b2a364f0a2f67ed81ad2f45f0ff09440d6
fiandinw <alifiandinursanni@gmail.com>
2022-06-02 18:31:30 +0700

    add print

--- src/views/AdminRekapView.vue
+++ src/views/AdminRekapView.vue
@@ -8,6 +8,8 @@
   } from "firebase/firestore";
   import { ref } from "vue";
   import { RouterLink } from "vue-router";
+  import printJS from "print-js";
+  import kopSurat from "../assets/kopSurat.png"
 
   const filterSelected = ref({
     asisten: "#",
@@ -33,7 +35,7 @@
 
   const dataLaporan = ref([]);
   const getLaporanData = () => {
-    dataLaporan.value = []
+    dataLaporan.value = [];
     const colref = collection(db, "laporan");
     const q = query(
       colref,
@@ -46,11 +48,17 @@
         snapshot.docs.forEach((each) => {
           dataLaporan.value.push({ ...each.data(), id: each.id });
         });
-        const arr = dataLaporan.value
-        arr.sort((a, b) => {return a.createdAt - b.createdAt})
-        dataLaporan.value = arr
+        const arr = dataLaporan.value;
+        arr.sort((a, b) => {
+          return a.createdAt - b.createdAt;
+        });
+        dataLaporan.value = arr;
       }
     });
+  };
+
+  const handlePrint = () => {
+    printJS({printable:"printContent", type:"html",targetStyles:['*'], style:".unhide {display: block !important}"});
   };
 </script>
 <template>
@@ -122,81 +130,94 @@
         type="button"
         class="px-6 py-2.5 w-fit bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
       >
-        <i class="fa fa-search fa-lg" aria-hidden="true"></i
-        >&nbsp;Cari Rekap Data
+        <i class="fa fa-search fa-lg" aria-hidden="true"></i>&nbsp;Cari Rekap
+        Data
       </button>
       <button
+        @click="handlePrint"
         type="button"
         class="px-6 py-2.5 w-fit bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
       >
-        <i class="fa fa-print fa-lg" aria-hidden="true"></i
-        >&nbsp;Cetak Data
+        <i class="fa fa-print fa-lg" aria-hidden="true"></i>&nbsp;Cetak Data
       </button>
     </div>
     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-        <div class="overflow-x-auto">
-          <table class="min-w-full">
-            <thead class="border-b">
+        <div id="printContent">
+          <div class="hidden unhide"><img class="" :src="kopSurat" alt="KopSurat"></div>
+          <div class="mt-8 hidden unhide">
+            <div v-if="filterSelected.asisten != '#'">Nama Asisten: {{ filterSelected.asisten != '#' && (asistenAktif.find((el) => (el.nim == filterSelected.asisten)).nama) }}</div>
+            <div v-if="filterSelected.createdMonth != '#' && filterSelected.createdYear != '#'">Tanggal Laporan: {{filterSelected.createdMonth}}/{{filterSelected.createdYear}}</div>
+          </div>
+          <table class="min-w-full mt-8">
+            <thead class="border-y">
               <tr>
                 <th
                   scope="col"
-                  class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  class="text-sm font-bold text-gray-900 px-2 py-4 text-left border-x-2"
                 >
                   #
                 </th>
                 <th
                   scope="col"
-                  class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  class="text-sm font-bold text-gray-900 px-2 py-4 text-left border-x-2"
                 >
                   Tanggal Laporan
                 </th>
                 <th
                   scope="col"
-                  class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  class="text-sm font-bold text-gray-900 px-2 py-4 text-left border-x-2"
                 >
                   Catatan
                 </th>
                 <th
                   scope="col"
-                  class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  class="text-sm font-bold text-gray-900 px-2 py-4 text-left border-x-2"
                 >
                   Luaran
                 </th>
                 <th
                   scope="col"
-                  class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  class="text-sm font-bold text-gray-900 px-2 py-4 text-left border-x-2"
                 >
                   Dokumentasi
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(item, key) in dataLaporan" :key="key" class="border-b">
+              <tr
+                v-for="(item, key) in dataLaporan"
+                :key="key"
+                class="border-b"
+              >
                 <td
-                  class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                  class="px-2 py-2 border-x-2 whitespace-nowrap text-sm font-medium text-gray-900"
                 >
-                  {{key+1}}
+                  {{ key + 1 }}
                 </td>
                 <td
-                  class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                  class="text-sm text-gray-900 font-light px-2 py-2 border-x-2 whitespace-nowrap"
                 >
-                  {{item.createdAt}}
+                  {{ item.createDay }}/{{ item.createMonth }}/{{ item.createYear }}
                 </td>
                 <td
-                  class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                  class="text-sm text-gray-900 font-light px-2 py-2 border-x-2 whitespace-nowrap"
                 >
-                  {{item.catatan}}
+                  {{ item.catatan }}
                 </td>
                 <td
-                  class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                  class="text-sm text-gray-900 font-light px-2 py-2 border-x-2 whitespace-nowrap"
                 >
-                  {{item.luaran}}
+                  {{ item.luaran }}
                 </td>
                 <td
-                  class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                  class="text-sm text-gray-900 font-light px-2 py-2 border-x-2 whitespace-nowrap"
                 >
-                  <img class="h-[50px] object-cover" :src="item.dokumentasi" alt="dokumentasi">
+                  <img
+                    class="h-[50px] object-cover"
+                    :src="item.dokumentasi"
+                    alt="dokumentasi"
+                  />
                 </td>
               </tr>
             </tbody>
